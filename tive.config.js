a4c6b18d3df964b94765102c84d30e0bb3c6a5fb
zhouxianfu <zhouxianfu@yiche.com>
2022-01-25 15:59:22 +0800

    update

--- tive.config.js
+++ tive.config.js
@@ -3,11 +3,11 @@ const desc = (argv.length > 4 && argv.slice(-1)[0]) || 'commit desc'
 
 module.exports = {
   shell: [
+    'npm run prettier',
     'git status',
     'git add .',
     `git commit -m "${desc}"`,
     'git push origin master',
-    'npm run prettier',
     'npm publish',
   ],
 }
